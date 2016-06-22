191b5718c811a670a5212743c8d26bd702f6929e
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-06-23 00:19:27 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const rp = require('minimal-request-promise')
+const botBuilder = require('claudia-bot-builder')
+const fbTemplate = botBuilder.fbTemplate
+
+module.exports = botBuilder(request => {
+  console.log(JSON.stringify(request))
+
+  if (!request.postback)
+    return rp({
+      method: 'GET',
+      hostname: 'graph.facebook.com',
+      path: `/v2.6/${request.sender}?fields=first_name,last_name,profile_pic,locale,timezone,gender&access_token=${request.env.facebookAccessToken}`,
+      port: 443
+    })
+      .then(response => {
+        const user = JSON.parse(response.body)
+        return [
+          `Hello ${user.first_name}, welcome to Space Explorer! Ready to start a new journey through the space?`,
+          'What can I do for you today?',
+          new fbTemplate.generic()
+            .addBubble(`NASA's Astronomy Picture of the Day`, 'Satellite icon by parkjisun from the Noun Project')
+              .addButton('Show', 'SHOW_APOD')
+              .addButton('Website', 'http://apod.nasa.gov/apod/')
+            .addBubble('Help & info')
+              .addButton('About the bot', 'ABOUT')
+              .addButton('Credits', 'CREDITS')
+            .get()
+        ]
+      })
+
+  if (request.text === 'SHOW_APOD')
+    return rp({
+      method: 'GET',
+      hostname: 'api.nasa.gov',
+      path: `/planetary/apod?api_key=${request.env.nasaApiKey}`,
+      port: 443
+    })
+      .then(response => {
+        const APOD = JSON.parse(response.body)
+        const img = new fbTemplate.image(APOD.url).get()
+        console.log(response.body)
+        return img
+      })
+})
