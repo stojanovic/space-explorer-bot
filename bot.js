d63012c6030c89f40cdf2c06e27741c09f98544a
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2016-12-27 11:14:49 +0100

    Add test config and first simple test

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const rp = require('minimal-request-promise')
 const botBuilder = require('claudia-bot-builder')
 const fbTemplate = botBuilder.fbTemplate
 const apiAi = require('./lib/helpers/api-ai')
+const mainMenu = require('./lib/main-menu')
 
 function getRoverPhotos(rover, sol, nasaApiKey) {
   if (!sol)
@@ -38,34 +39,6 @@ function getRoverPhotos(rover, sol, nasaApiKey) {
       console.log(err)
       return getRoverPhotos(rover, 1000, nasaApiKey)
     })
-}
-
-function mainMenu() {
-  return new fbTemplate.generic()
-    .addBubble(`NASA's Astronomy Picture of the Day`, 'Satellite icon by parkjisun from the Noun Project')
-      .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/apod.png')
-      .addButton('Show', 'SHOW_APOD')
-      .addButton('What is APOD?', 'ABOUT_APOD')
-      .addButton('Website', 'http://apod.nasa.gov/apod/')
-    .addBubble(`Photos from NASA's rovers on Mars`, 'Curiosity Rover icon by Oliviu Stoian from the Noun Project')
-      .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/rovers.png')
-      .addButton('Curiosity', 'CURIOSITY_IMAGES')
-      .addButton('Opportunity', 'OPPORTUNITY_IMAGES')
-      .addButton('Spirit', 'SPIRIT_IMAGES')
-    .addBubble('International Space Station', 'Space station icon by Lucid Formation from the Noun Project')
-      .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/iss.png')
-      .addButton('Current position', 'ISS_POSITION')
-      .addButton('Website', 'https://www.nasa.gov/mission_pages/station/')
-    .addBubble('How many people are in Space right now?', 'Astronaut icon by Javier Cabezas from the Noun Project')
-      .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/astronaut.png')
-      .addButton('Show', 'PEOPLE_IN_SPACE')
-      .addButton('Website', 'http://www.howmanypeopleareinspacerightnow.com')
-    .addBubble('Help & info', 'Monster icon by Paulo Sá Ferreira from the Noun Project')
-      .addImage('https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/about.png')
-      .addButton('About the bot', 'ABOUT')
-      .addButton('Credits', 'CREDITS')
-      .addButton('Report an issue', 'https://github.com/stojanovic/space-explorer-bot/issues')
-    .get()
 }
 
 const api = botBuilder((request, originalApiRequest) => {
---
+++ spec/main-menu-spec.js
@@ -0,0 +1,100 @@
+/* global describe, it, expect */
+'use strict'
+
+const mainMenu = require('../lib/main-menu')
+const expected = {
+  attachment: {
+    type: 'template',
+    payload: {
+      template_type:'generic',
+      elements: [{
+        title: 'NASA\'s Astronomy Picture of the Day',
+        subtitle: 'Satellite icon by parkjisun from the Noun Project',
+        image_url: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/apod.png',
+        buttons: [{
+          title: 'Show',
+          type: 'postback',
+          payload: 'SHOW_APOD'
+        }, {
+          title: 'What is APOD?',
+          type: 'postback',
+          payload: 'ABOUT_APOD'
+        }, {
+          title: 'Website',
+          type: 'web_url',
+          url: 'http://apod.nasa.gov/apod/'
+        }]
+      }, {
+        title: 'Photos from NASA\'s rovers on Mars',
+        subtitle: 'Curiosity Rover icon by Oliviu Stoian from the Noun Project',
+        image_url: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/rovers.png',
+        buttons: [{
+          title: 'Curiosity',
+          type: 'postback',
+          payload: 'CURIOSITY_IMAGES'
+        }, {
+          title: 'Opportunity',
+          type: 'postback',
+          payload: 'OPPORTUNITY_IMAGES'
+        }, {
+          title: 'Spirit',
+          type: 'postback',
+          payload: 'SPIRIT_IMAGES'
+        }]
+      }, {
+        title: 'International Space Station',
+        subtitle: 'Space station icon by Lucid Formation from the Noun Project',
+        image_url: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/iss.png',
+        buttons: [{
+          title: 'Current position',
+          type: 'postback',
+          payload: 'ISS_POSITION'
+        }, {
+          title: 'Website',
+          type: 'web_url',
+          url: 'https://www.nasa.gov/mission_pages/station/'
+        }]
+      }, {
+        title: 'How many people are in Space right now?',
+        subtitle: 'Astronaut icon by Javier Cabezas from the Noun Project',
+        image_url: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/astronaut.png',
+        buttons: [{
+          title: 'Show',
+          type: 'postback',
+          payload: 'PEOPLE_IN_SPACE'
+        }, {
+          title: 'Website',
+          type: 'web_url',
+          url: 'http://www.howmanypeopleareinspacerightnow.com'
+        }]
+      }, {
+        title: 'Help & info',
+        subtitle: 'Monster icon by Paulo Sá Ferreira from the Noun Project',
+        image_url: 'https://raw.githubusercontent.com/stojanovic/space-explorer-bot/master/assets/images/about.png',
+        buttons: [{
+          title: 'About the bot',
+          type: 'postback',
+          payload: 'ABOUT'
+        }, {
+          title: 'Credits',
+          type: 'postback',
+          payload: 'CREDITS'
+        }, {
+          title: 'Report an issue',
+          type: 'web_url',
+          url: 'https://github.com/stojanovic/space-explorer-bot/issues'
+        }]
+      }]
+    }
+  }
+}
+
+describe('Main menu', () => {
+  it('should be a function', () => {
+    expect(typeof mainMenu).toBe('function')
+  })
+
+  it('should transform the menu to the expected json', () => {
+    expect(mainMenu()).toEqual(expected)
+  })
+})
